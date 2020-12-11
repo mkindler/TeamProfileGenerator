65866dc64d06193771604317a0f4e755009d4029
Mae Kindler <mae.akie.kindler@gmail.com>
2020-12-10 22:47:20 -0600

    Wrote code for a function that will create the Manager within the team.

--- app.js
+++ app.js
@@ -13,6 +13,75 @@ const render = require("./lib/htmlRenderer");
 const teamMembers = [];
 const idArray = [];
 
+function teamMenu() {
+
+    function createManager() {
+        console.log("It's time to build your team");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is your manager's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character to proceed.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is your manager's ID?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a number greater than zero.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is your manager's email address?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /\S+@\S+\.\S+/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a valide email address.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is your manager's office number?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a number greater than zero.";
+                }
+            }
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamMembers.push(manager);
+            idArray.push(answers.managerId);
+            createTeam();
+        });
+    }
+
+    
+}
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
