b858822b511137939d6a5bbfcadff70864ce0afc
Mae Kindler <mae.akie.kindler@gmail.com>
2020-12-10 23:02:33 -0600

    Write the function to create an engineer.

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function teamMenu() {
             if (pass) {
               return true;
             }
-            return "Please enter a number greater than zero.";
+            return "Please enter a positive number greater than zero.";
           },
         },
         {
@@ -62,7 +62,7 @@ function teamMenu() {
             if (pass) {
               return true;
             }
-            return "Please enter a number greater than zero.";
+            return "Please enter a positive number greater than zero.";
           },
         },
       ])
@@ -106,6 +106,70 @@ function teamMenu() {
         }
       });
   }
+
+  function addEngineer() {
+      inquirer.prompt([
+          {
+              type: "input",
+              name: "engineerName",
+              message: "What is your engineer's name?",
+              validate: answer => {
+                  if (answer !== "") {
+                      return true;
+                  }
+                  return "Please enter at least one character to proceed.";
+              }
+          },
+          {
+              type: "input",
+              name: "engineerId",
+              message: "What is your engineer's ID?",
+              validate: answer => {
+                  const pass = answer.match(
+                      /^[1-9]\d*$/
+                  );
+                  if (pass) {
+                      if (idArray.includes(answer)) {
+                          return "This ID is already in use. Please enter a different number.";
+                      } else {
+                          return true;
+                      }
+                  }
+                  return "Please enter a positive number greater than zero."
+              }
+          },
+          {
+              type: "input",
+              name: "engineerEmail",
+              message: "What is your engineer's email address?",
+              validate: (answer) => {
+                const pass = answer.match(/\S+@\S+\.\S+/);
+                if (pass) {
+                  return true;
+                }
+                return "Please enter a valide email address.";
+              },
+          },
+          {
+              type: "input",
+              name: "engineerGithub",
+              message: "What is your engineer's GitHub username?",
+              validate: (answer) => {
+                if (answer !== "") {
+                  return true;
+                }
+                return "Please enter at least one character to proceed.";
+              },
+          }
+      ]).then(answers => {
+          const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+          teamMembers.push(engineer);
+          idArray.push(answers.engineerId);
+          createTeam();
+      });
+  }
+
+  
 }
 
 // Write code to use inquirer to gather information about the development team members,
